222dc26f307fe446ab520b7e776843dc440385e2
marcadrian-it <marcadrian.it@gmail.com>
2023-06-14 09:55:10 +0200

    journal page

---
+++ app/new-user/loading.tsx
@@ -0,0 +1,7 @@
+export default function NewUserLoader() {
+  return (
+    <div className="loading-pane flex items-center justify-center p-4">
+      <h2 className="loader text-9xl animate-spin">🌀</h2>
+    </div>
+  );
+}
--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,29 @@
-const NewUser = () => {
-  return <div>New User</div>;
+import { prisma } from '@/utils/db';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const createNewUser = async () => {
+  const user = await currentUser();
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  });
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    });
+  }
+
+  redirect('/journal');
+};
+
+const NewUser = async () => {
+  await createNewUser();
+  return <div></div>;
 };
 
 export default NewUser;
